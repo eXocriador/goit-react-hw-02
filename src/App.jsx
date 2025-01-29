7ecc73ce44b7a88627986273b9b0e26a354ccb87
Oleh Tatarynov <olehtatarynov@MacBook-Pro-Oleh.local>
2025-01-29 22:14:44 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,18 @@ import Options from "./components/Options/Options";
 import "./App.css";
 
 function App() {
+  const feedbackType = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  const [feedback, setFeedback] = useState(feedbackType);
+  const handleFeedback = (type) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [type]: prevFeedback[type] + 1,
+    }));
+  };
   return (
     <>
       <Description />
